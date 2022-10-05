4bcbb6aa0cb8741600a81dd8b00c44d2716cd058
DESKTOP-JMQ6SB4\Aleksia <alexiabega0930@gmal.com>
2022-10-05 18:05:18 +0200

    main activity layout

--- app/src/main/java/com/example/stationprogram/MainActivity.kt
+++ app/src/main/java/com/example/stationprogram/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.stationprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stationprogram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addProgramButton.setOnClickListener {  }
+
+        binding.bottomNavigation.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.station_one -> readData()
+                else -> readData()
+            }
+            true
+        }
+    }
+
+    private fun readData(){
     }
 }
